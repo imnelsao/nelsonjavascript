960f643c70798b2c955ba84c18684bae1ce95a78
Nelson Souza <nelson.dias@outlook.com>
2024-05-09 21:20:49 -0300

    subindo arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+var texto = prompt("Digite um texto:");
+var numero = parseFloat(prompt("Digite um número:"));
+var textoConcatenado = texto + " com concatenação.";
+var resultadoMatematico = numero + 10;
+alert("Resultado da concatenação: " + textoConcatenado);
+console.log("Resultado da operação matemática: " + resultadoMatematico);
