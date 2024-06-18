3642197f0cf6ca38b962fffd665c6caf0f3fbbd4
Nelson Souza <nelson.dias@outlook.com>
2024-06-18 18:39:03 -0300

    arrays

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class Emprestimo {
 		this.valorTotal = parseFloat(valorTotal);
 		this.quantidadeParcelas = parseInt(quantidadeParcelas);
 		this.resultDiv = document.getElementById("result");
+		this.parcelas = [];
 	}
 
 	validarEntrada() {
@@ -27,12 +28,36 @@ class Emprestimo {
 		let parcelaAtual = this.valorTotal / this.quantidadeParcelas;
 		const decremento = parcelaAtual / this.quantidadeParcelas;
 
+		this.parcelas = [];
+
 		for (let i = 1; i <= this.quantidadeParcelas; i++) {
+			this.parcelas.push(parcelaAtual.toFixed(2));
 			this.resultDiv.innerHTML += `<p>Parcela ${i}: R$${parcelaAtual.toFixed(
 				2
 			)}</p>`;
 			parcelaAtual -= decremento;
 		}
+
+		this.exibirResultadosAdicionais();
+	}
+
+	calcularSomaParcelas() {
+		return this.parcelas.reduce((acc, parcela) => acc + parseFloat(parcela), 0);
+	}
+
+	calcularMediaParcelas() {
+		return this.calcularSomaParcelas() / this.parcelas.length;
+	}
+
+	exibirResultadosAdicionais() {
+		const somaParcelas = this.calcularSomaParcelas();
+		const mediaParcelas = this.calcularMediaParcelas();
+		this.resultDiv.innerHTML += `<p>Soma das Parcelas: R$${somaParcelas.toFixed(
+			2
+		)}</p>`;
+		this.resultDiv.innerHTML += `<p>Média das Parcelas: R$${mediaParcelas.toFixed(
+			2
+		)}</p>`;
 	}
 }
 
